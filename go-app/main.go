2af9d411962d6b374dc3beb02f620183e7160389
Håvar Eggereide <haavar.e@gmail.com>
2018-11-27 23:39:30 +0100

    go-app: app with just metrics endpoint
     #5

---
+++ go-app/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp:processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
+
+func main() {
+	recordMetrics()
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
+}
+
+func recordMetrics() {
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
