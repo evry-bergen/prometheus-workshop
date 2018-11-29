ce8982e470e27ab7d5b7dcb7a8fd2ca65b78ef64
Hans Kristian Flaatten <hans.flaatten@evry.com>
2018-11-29 17:53:56 +0100

    Open firewall for node_exporter and Prometheus

--- terraform/modules/machine/main.tf
+++ terraform/modules/machine/main.tf
@@ -38,6 +38,30 @@ resource "azurerm_network_security_group" "vm" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "Prometheus"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9090"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "node_exporter"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9100"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags {
     environment = "${var.environment}"
   }
