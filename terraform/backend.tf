eb1a6e168dd29790ecf8f37376ed1ec241d8d175
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-03-25 13:19:06 +0100

    Update storage account name for Terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "promwsterraformstate"
+    storage_account_name = "prometheusws"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
