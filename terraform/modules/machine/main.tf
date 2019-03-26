37eaaa39c56ba48b55faf010844ba5d9d57c1092
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-03-26 22:12:52 +0100

    Allow traffic over port 3000 to Grafana

--- terraform/modules/machine/main.tf
+++ terraform/modules/machine/main.tf
@@ -62,6 +62,18 @@ resource "azurerm_network_security_group" "vm" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "grafana"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags {
     environment = "${var.environment}"
   }
